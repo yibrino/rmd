6abe4a91b86359c402cf74a2291a9dce7465e5c4
yibrino <51756024+yibrino@users.noreply.github.com>
2023-10-08 22:58:49 +0200

    Create sum_numbers.cpp

---
+++ sum_numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    int number, sum = 0;
+
+    while (true) {
+        std::cout << "Enter a positive integer (0 to exit): ";
+        std::cin >> number;
+
+        if (number == 0) {
+            break;
+        }
+
+        if (number < 0) {
+            std::cout << "Please enter a positive integer." << std::endl;
+            continue;
+        }
+
+        sum += number;
+    }
+
+    std::cout << "Sum of the positive integers: " << sum << std::endl;
+
+    return 0;
+}
